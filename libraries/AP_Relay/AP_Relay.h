51400c334bf489b209581454b57679bcf85cd358
Michael du Breuil <wicked.shell.scripts@gmail.com>
2023-09-26 14:58:26 -0700

    AP_Relay: Refactor to support RELAYx_FUNCTION

--- libraries/AP_Relay/AP_Relay.h
+++ libraries/AP_Relay/AP_Relay.h
@@ -10,6 +10,8 @@
 #pragma once
 
 #include <AP_Param/AP_Param.h>
+#include <AP_Relay/AP_Relay_Params.h>
+#include <AP_Common/Bitmask.h>
 
 #define AP_RELAY_NUM_RELAYS 6
 
@@ -32,12 +34,10 @@ public:
     void        off(uint8_t instance) { set(instance, false); }
 
     // get state of relay
-    uint8_t     get(uint8_t instance) const {
-        return instance < AP_RELAY_NUM_RELAYS ? _pin_states & (1U<<instance) : 0;
-    }
+    bool        get(uint8_t instance) const;
     
     // see if the relay is enabled
-    bool        enabled(uint8_t instance) { return instance < AP_RELAY_NUM_RELAYS && _pin[instance] != -1; }
+    bool        enabled(uint8_t instance) const { return instance < AP_RELAY_NUM_RELAYS && _params[instance].pin != -1; }
 
     // toggle the relay status
     void        toggle(uint8_t instance);
@@ -49,16 +49,21 @@ public:
 
     static const struct AP_Param::GroupInfo        var_info[];
 
+    bool send_relay_status(const class GCS_MAVLINK &link) const;
+
+    void set(AP_Relay_Params::Function function, bool value);
+
 private:
     static AP_Relay *singleton;
 
-    AP_Int8 _pin[AP_RELAY_NUM_RELAYS];
-    AP_Int8 _default;
-    uint8_t _pin_states;
-    uint8_t _last_logged_pin_states;
-    uint32_t _last_log_ms;
+    AP_Relay_Params _params[AP_RELAY_NUM_RELAYS];
 
     void set(uint8_t instance, bool value);
+
+    void set_defaults();
+    void convert_params();
+
+    void set_pin_by_instance (uint8_t instance, bool value);
 };
 
 namespace AP {
