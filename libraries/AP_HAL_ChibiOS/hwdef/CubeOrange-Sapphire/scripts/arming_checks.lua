fb43f2d7fe9d97be9f3289fe9264a5d0c0f4cd25
Michael du Breuil <wicked.shell.scripts@gmail.com>
2024-02-22 15:34:47 -0700

    AP_HAL_Chibios: Fix arming script

--- libraries/AP_HAL_ChibiOS/hwdef/CubeOrange-Sapphire/scripts/arming_checks.lua
+++ libraries/AP_HAL_ChibiOS/hwdef/CubeOrange-Sapphire/scripts/arming_checks.lua
@@ -19,23 +19,25 @@ local batt_temp_mask = bind_add_param("_BAT_TEMP_MSK", 3, 6)
 local auth_id = arming:get_aux_auth_id()
 
 function update()
-  if auth_id then    
-    local all_passing = true 
+  if auth_id then
+    local all_passing = true
     -- battery temp checks
     local batt_mask = batt_temp_mask:get()
+    local temp_min = batt_temp_min:get()
+    local temp_max = batt_temp_max:get()
     for i=0,10 do
-      if batt_mask & (1 << i) then
-        batt_temp = battery:get_temperature(i)    
-        if not batt_temp then    
-          arming:set_aux_auth_failed(auth_id, "Could not retrieve battery" .. i + 1 .. " temperature")    
+      if (batt_mask & (1 << i)) > 0 then
+        batt_temp = battery:get_temperature(i)
+        if not batt_temp then
+          arming:set_aux_auth_failed(auth_id, "Could not retrieve battery" .. i + 1 .. " temperature")
           all_passing = false
           break
-        elseif (batt_temp > batt_temp_max) then    
-          arming:set_aux_auth_failed(auth_id, "Battery " .. i + 1 .. " temp too high (" .. tostring(batt_temp) .. "C > " .. tostring(batt_temp_max) .. "C)")    
+        elseif (batt_temp > temp_max) then
+          arming:set_aux_auth_failed(auth_id, string.format("Battery %d temp too high (%.1f > %.0f C)", i + 1, batt_temp, temp_max))
           all_passing = false
           break
-        elseif (batt_temp < batt_temp_low) then    
-          arming:set_aux_auth_failed(auth_id, "Battery " .. i + 1 .. " temp too low (" .. tostring(batt_temp) .. "C > " .. tostring(batt_temp_low) .. "C)")    
+        elseif (batt_temp < temp_min) then
+          arming:set_aux_auth_failed(auth_id, string.format("Battery %d temp too low (%.1f < %.0f C)", i + 1, batt_temp, temp_min))
           all_passing = false
           break
         end
